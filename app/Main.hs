e16e66b11142a1a97733ce9aff4b5ea60f63ab1c
carlows <carlosandrade@ciencias.unam.mx>
2023-06-06 02:08:39 -0600

    Leer archivos jala

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,20 @@
-module Main where
+import System.Environment
+import System.IO
 
+
+
+-- FUNCION PRINCIPAL
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  -- Obtenemos los argumentos
+  args <- getArgs
+  case args of
+    [file] -> do
+      -- Manipulamos al archivo aqui 
+      putStrLn $ "Archivo proporcionado: " ++ file
+      contents <- readFile file
+      putStrLn contents
+
+    -- En caso de que no te proporcionen un archivo 
+    _ -> putStrLn "Se debe proporcionar un archivo como argumento"
+
