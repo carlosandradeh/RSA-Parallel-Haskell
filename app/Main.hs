a26da6ca9633462736c1e49fa9bf81bfc3b125e6
carlows <carlosandrade@ciencias.unam.mx>
2023-06-08 13:02:17 -0600

    Encriptacion

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,7 @@ import System.Random
 
 -- FUNCION PRINCIPAL
 main :: IO ()
-main = do
+main = do 
   -- Obtenemos los argumentos
   args <- getArgs
   case args of
@@ -15,6 +15,23 @@ main = do
       contents <- readFile file
       putStrLn contents
       putStrLn $ show (primos 40)
+      -- putStrLn $ show (generaLlaves 80000000)
+      let llavePublica = 7
+      let llavePrivada = 23
+      let n = 187
+
+      let cadena = "Hola cara de verga, te odio y te desprecio\nHueles a mierda rompe madres"
+      putStrLn $ "\nTexto original: " ++ cadena
+      
+      -- Encriptar
+      let textoEncriptadoCadena = encriptaCadena llavePublica n cadena
+      putStrLn $ "Texto encriptado (cadena): " ++ show textoEncriptadoCadena
+      
+      -- Desencriptar
+      let textoDesencriptadoCadena = desencriptaCadena llavePrivada n textoEncriptadoCadena
+      putStrLn $ "Texto desencriptado (cadena): " ++ textoDesencriptadoCadena
+
+
     -- En caso de que no te proporcionen un archivo 
     _ -> putStrLn "Se debe proporcionar un archivo como argumento"
 
