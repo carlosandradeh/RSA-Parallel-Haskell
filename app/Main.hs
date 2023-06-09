79c7f0734b0af8840855c79f749191df6de28be8
carlows <carlosandrade@ciencias.unam.mx>
2023-06-09 05:09:11 -0600

    Encriptado y desencriptado

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,11 @@
 import System.Environment
 import System.IO
-import RSA
 import System.Random
+import RSA
+
+appendLinesToFile :: FilePath -> [String] -> IO ()
+appendLinesToFile filePath lines = do
+  mapM_ (appendFile filePath) lines
 
 -- FUNCION PRINCIPAL
 main :: IO ()
@@ -13,25 +17,33 @@ main = do
       -- Manipulamos al archivo aqui 
       putStrLn $ "Archivo proporcionado: " ++ file
       contents <- readFile file
-      putStrLn contents
-      putStrLn $ show (primos 40)
+      -- putStrLn contents
+      --putStrLn $ show (primos 40)
       -- putStrLn $ show (generaLlaves 80000000)
-      let llavePublica = 7
-      let llavePrivada = 23
-      let n = 187
 
-      let cadena = "Hola cara de verga, te odio y te desprecio\nHueles a mierda rompe madres"
-      putStrLn $ "\nTexto original: " ++ cadena
-      
+      putStrLn $ "\nTexto original: " ++ contents
+
+      -- Generar llaves !!
+      putStrLn "Ingresa un numero cuaquiera para generar llaves:"
+      semilla <- getLine
+      let (llavePublica, llavePrivada, n) = generaLlaves (read semilla :: Int)
+      let lineas = ["\nLlave Publica: " ++ show llavePublica, "\nLlave Privada: " ++ show llavePrivada, "\nn: " ++ show n]
+      appendLinesToFile "llaves.txt" lineas
+      putStrLn "Se ha generado el archivo con las LLAVES correctamente."
+
+
       -- Encriptar
-      let textoEncriptadoCadena = encriptaCadena llavePublica n cadena
-      putStrLn $ "Texto encriptado (cadena): " ++ show textoEncriptadoCadena
-      
+      let textoEncriptadoCadena = encriptaCadena llavePublica n contents
+      --putStrLn $ "Texto encriptado (cadena): " ++ show textoEncriptadoCadena
+      writeFile "encriptado.txt" (show textoEncriptadoCadena)
+      putStrLn "Se ha generado el archivo encriptado correctamente."
+
       -- Desencriptar
       let textoDesencriptadoCadena = desencriptaCadena llavePrivada n textoEncriptadoCadena
-      putStrLn $ "Texto desencriptado (cadena): " ++ textoDesencriptadoCadena
-
-
+      --putStrLn $ "Texto desencriptado (cadena): " ++ textoDesencriptadoCadena
+      writeFile "desencriptado.txt" textoDesencriptadoCadena
+      putStrLn "Se ha generado el archivo desencriptado correctamente."
+    
     -- En caso de que no te proporcionen un archivo 
     _ -> putStrLn "Se debe proporcionar un archivo como argumento"
 
